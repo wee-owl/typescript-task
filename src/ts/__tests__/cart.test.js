e149ab4dfac2d0b518ee7be1b24a8910ff1365dc
wee-owl <kem-cv@yandex.ru>
2023-06-14 11:08:15 +0300

    feat: add second task

--- src/ts/__tests__/cart.test.js
+++ src/ts/__tests__/cart.test.js
@@ -7,11 +7,43 @@ test('new cart should be empty', () => {
 
 test('checking the addition of good to the cart', () => {
   const cart = new Cart();
-  const good = {
-    id: 100,
-    name: test,
-    price: 1000,
-  };
+  const good = { id: 100, name: test, price: 1000 };
   cart.add(good);
   expect(cart.items).toEqual([good]);
+});
+
+test('checking the total value of the item in the cart', () => {
+  const cart = new Cart();
+  cart.add({ id: 114, name: 'The War of the Worlds', price: 1268 });
+  cart.add({ id: 231, name: "We'll Meet Again", price: 2126 });
+  cart.add({ id: 397, name: 'The Avengers', price: 954 });
+  expect(cart.sum()).toBe(4348);
+});
+
+test('checking the apply discount to the total cost of the item in the cart', () => {
+  const cart = new Cart();
+  cart.add({ id: 114, name: 'The War of the Worlds', price: 1268 });
+  cart.add({ id: 231, name: "We'll Meet Again", price: 2126 });
+  cart.add({ id: 397, name: 'The Avengers', price: 954 });
+  expect(cart.sumDiscount(15)).toBe(3695.8);
+});
+
+test('checking the removal of an item from the cart', () => {
+  const cart = new Cart();
+  cart.add({ id: 114, name: 'The War of the Worlds', price: 1268 });
+  cart.add({ id: 231, name: "We'll Meet Again", price: 2126 });
+  cart.add({ id: 397, name: 'The Avengers', price: 954 });
+  cart.deleteGood(231);
+  expect(cart.items).toEqual([
+    { id: 114, name: 'The War of the Worlds', price: 1268 },
+    { id: 397, name: 'The Avengers', price: 954 },
+  ]);
+});
+
+test('checking for removal of invalid product id from cart', () => {
+  const cart = new Cart();
+  cart.add({ id: 114, name: 'The War of the Worlds', price: 1268 });
+  cart.add({ id: 231, name: "We'll Meet Again", price: 2126 });
+  cart.add({ id: 397, name: 'The Avengers', price: 954 });
+  expect(() => cart.deleteGood(111)).toThrow();
 });
--- src/ts/cart.ts
+++ src/ts/cart.ts
@@ -10,4 +10,18 @@ export default class Cart {
   get items(): Buyable[] {
     return [...this._items];
   }
+
+  sum(): number {
+    return this._items.reduce((acc: number, cur: Buyable) => acc + cur.price, 0);
+  }
+
+  sumDiscount(discount: number): number {
+    return +(this.sum() * (1 - 0.01 * discount)).toFixed(2);
+  }
+
+  deleteGood(id: number): void {
+    const index = this._items.findIndex(item => item.id === id);
+    if (index === -1) throw new Error('Invalid product id');
+    this._items.splice(index, 1);
+  }
 }
